1fb021e3e014d8ca84b52f73c1fe290cb6960d78
Ahmed abdalrahman <90632713+AHMED-JMD@users.noreply.github.com>
2022-09-27 16:23:38 +0200

    Delete Erm factory directory

--- Erm factory/routes/users/signup.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const user = require("../../controllers/users/usersController");
-
-router.post("/", user.signup);
-
-module.exports = router;
